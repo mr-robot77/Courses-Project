46888cb7d7eb08e2f8122c0e08cd4ed644954843
Saleh Abednezhad <dj.abed765@gmail.com>
2023-06-15 19:49:50 +0330

    Update returner.py

--- Python/ch2.String/returner.py
+++ Python/ch2.String/returner.py
@@ -1,16 +1,12 @@
 # Read_input():
 k = int(input()) 
 code = input()
-locks = []
-for i in range(k):
-    locks.append(list(map(int,input().split())))  
+def locks(char , number):
+    position= number.find(char)
+    return min(position , len(number)-position)
 
 # Calculate_minimum_turns
-minimum_turns = 0   
+minimum_turns = 0
 for i in range(k):
-    number = int(code[i])
-    position = 0
-    if number in locks[i]:
-        position = locks[i].index(number)
-        minimum_turns += abs(position - int(locks[i][0]))
+    minimum_turns += locks(code[i], input())
 print(minimum_turns)
