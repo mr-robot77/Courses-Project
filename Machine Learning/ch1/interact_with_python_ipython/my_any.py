c1bd4c14f493952077f26655fd0d90a13c9e6ab4
Saleh Abednezhad <abednezhad756@gmail.com>
2023-06-26 23:15:33 +0330

    Add files via upload

---
+++ Machine Learning/ch1/interact_with_python_ipython/my_any.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+def my_any(x):
+    for i in x:
+        if i:
+            return True
+    return False
---
+++ Machine Learning/ch1/interact_with_python_warm_up/hello_quera.py
@@ -0,0 +1,3 @@
+# coding: utf-8
+def hello_quera():
+    return  'Hello Quera'
