31469bebd0a7e1543f53511fac145f604f997288
Saleh Abednezhad <dj.abed765@gmail.com>
2023-06-26 23:06:43 +0330

    Update hard compution.py

--- Python/ch1.Review/hard compution.py
+++ Python/ch1.Review/hard compution.py
@@ -1,4 +1,5 @@
 from math import *
 x = int(input())
-y = floor(pow(e, asin(pow(cos(radians(x)), 2))))
-print(gcd(x, y))
+y = ceil(pow(x,5/3)+tan(radians(x)))
+z = floor(pow(pi, 2+atan(pow(sin(radians(x)),2))))
+print(gcd(y, z))
