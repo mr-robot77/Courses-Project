c0ce3be50c9e177d3033a559b9530321e99f6481
Saleh Abednezhad <abednezhad756@gmail.com>
2023-07-16 11:38:09 +0330

    Add files via upload

---
+++ Python/ch6.OOP/naghel/airplane.py
@@ -0,0 +1,14 @@
+from ground_vehicle import GroundVehicle
+from flying_vehicle import FlyingVehicle
+
+
+class Airplane(GroundVehicle,FlyingVehicle):
+    def __init__(self, airline, number_of_crew, captain, **kwargs):
+        self.airline = airline
+        self.number_of_crew = number_of_crew
+        self.captain = captain
+        super().__init__(**kwargs)
+
+class B707(Airplane):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
